4545e8a5db8b77b156bfe56862d3dfee281281ef
whoami <1363531458@qq.com>
2019-11-06 09:47:57 +0800

    删除不必要的pom,增加资源监控模块

--- src/main/java/run/app/AppApplication.java
+++ src/main/java/run/app/AppApplication.java
@@ -1,8 +1,8 @@
 package run.app;
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
